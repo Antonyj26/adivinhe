2d22c1e6432705912f1dde00cd57129e509019a1
Antonyj26 <antonyjosu26@gmail.com>
2025-11-27 23:37:48 -0300

    Alteraçãode de tema

--- src/utils/words.ts
+++ src/utils/words.ts
@@ -5,13 +5,39 @@ export type Challenge = {
 };
 
 export const WORDS: Challenge[] = [
-  { id: 1, word: "LIA", tip: "Primeira mulher de Jacó" },
-  { id: 2, word: "JOSE", tip: "Foi o governador do Egito" },
-  { id: 3, word: "MOISES", tip: "Ele viu a sarça ardente" },
+  // { id: 1, word: "LIA", tip: "Primeira mulher de Jacó" },
+  // { id: 2, word: "JOSE", tip: "Foi o governador do Egito" },
+  // { id: 3, word: "MOISES", tip: "Ele viu a sarça ardente" },
+  // {
+  //   id: 4,
+  //   word: "JOSUE",
+  //   tip: "O escolhido para substituir Moises",
+  // },
+  // { id: 5, word: "DAVI", tip: "Homem segundo o coração de Deus" },
+  // { id: 1, word: "LUGANO", tip: "Ídolo uruguaio conhecido pela raça em campo" },
+  { id: 1, word: "LUGANO", tip: "Ídolo uruguaio conhecido pela raça em campo" },
+
+  // { id: 2, word: "TELÊ", tip: "Treinador lendário que comandou o time nas conquistas mundiais" },
   {
-    id: 4,
-    word: "JOSUE",
-    tip: "O escolhido para substituir Moises",
+    id: 2,
+    word: "TELE",
+    tip: "Treinador lendário que comandou o time nas conquistas mundiais",
   },
-  { id: 5, word: "DAVI", tip: "Homem segundo o coração de Deus" },
+
+  // { id: 3, word: "CICINHO", tip: "Lateral-direito importante na conquista da Libertadores de 2005" },
+  {
+    id: 3,
+    word: "CICINHO",
+    tip: "Lateral-direito importante na conquista da Libertadores de 2005",
+  },
+
+  // { id: 4, word: "MORUMBI", tip: "Estádio histórico inaugurado em 1960" },
+  { id: 4, word: "MORUMBI", tip: "Estádio histórico inaugurado em 1960" },
+
+  // { id: 5, word: "RAI", tip: "Um dos maiores ídolos da história, campeão mundial em 1992" }
+  {
+    id: 5,
+    word: "RAI",
+    tip: "Um dos maiores ídolos da história, campeão mundial em 1992",
+  },
 ];
